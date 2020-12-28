fb690b136c1e5915304bcef88a4b9386e02bff9a
Dragon <438369085@qq.com>
2020-12-28 22:15:08 +0800

    Add underscore plugin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import _u from 'underscore'
 
 function App() {
+  const arr = ['Hi ', ' Abo']
+  const str = _u.join(arr)
   return (
     <div className="App">
       <header className="App-header">
         <h2>Hello feature</h2>
+        <p>God:{str}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
